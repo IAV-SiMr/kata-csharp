999bd9fd5d96c943fc72b17cf79446150f02a859
Falk Kühnel <falk@kuehnel.eu>
2023-06-13 20:52:42 +0200

    Unit Test for Kata

---
+++ KataUnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace KataUnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.True(false);
+    }
+}
